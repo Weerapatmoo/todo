3d659ece638e50a876fde921999ff07328ff1aad
Weerapatmoo <sisocen@gmail.com>
2020-03-19 13:38:44 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const Config = {
+        apiKey: "AIzaSyBSoaJ_CmQLvKudZr8eBXtb7w1Ln9iVFfo",
+        authDomain: "todo-d93ea.firebaseapp.com",
+        databaseURL: "https://todo-d93ea.firebaseio.com",
+        projectId: "todo-d93ea",
+        storageBucket: "todo-d93ea.appspot.com",
+        messagingSenderId: "291605398316",
+        appId: "1:291605398316:web:834da3e3a037f78d3f2289",
+        measurementId: "G-048DRVZ6JN"
+      };
+    
+export default Config;
